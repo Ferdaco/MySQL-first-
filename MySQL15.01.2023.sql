edb4a56472423d8102abeb5118f295a022bd1244
Ramazan Koçum <117107446+Ferdaco@users.noreply.github.com>
2023-01-16 00:53:32 +0300

    Add files via upload
    MySQL dosyası oluşturuldu.

---
+++ MySQL15.01.2023.sql
@@ -0,0 +1,10 @@
+INSERT INTO clarusway.Personal_Info_1
+(ID_number, Name, Surname, Gender, Salary)
+VALUES
+('1234','Osvaldo','Clarusway','Male','45000'), 
+('56789','Guile','Clarusway','Male','55000'), 
+('007','Charlie','Clarusway','Male','50000'), 
+('432','Marcus','Clarusway','Male','55000'), 
+('324','Vincenzo','Clarusway','Male','65000'),
+('4587','Aslan','Clarusway','Male','40000'), 
+('43546','Serdar','Clarusway','Male','70000');
